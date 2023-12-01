23978c2b2416c5be2b088c57890198cbabc264c2
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-12-01 20:34:22 +0100

    Day 1 : Implemented first task

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,25 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[get("/")]
+#[get("/error")]
 fn error() -> http::Status {
     http::Status::InternalServerError
 }
 
 
+#[get("/<num1>/<num2>")]
+fn xor(num1: u16, num2: u16) -> String {
+    let xor = num1 ^ num2;
+    let result: usize = xor.pow(3) as usize;
+    format!("{}", result)
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_rocket::ShuttleRocket {
     let rocket = rocket::build()
         .mount("/", routes![index ])
-        .mount("/-1/error", routes![error]);
+        .mount("/-1", routes![error])
+        .mount("/1", routes![xor]);
 
     Ok(rocket.into())
 }
